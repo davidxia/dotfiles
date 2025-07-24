adc96781fd41313b544a93fe43af2218ffc66901
David Xia <david@davidxia.com>
2025-07-24 10:29:24 -0400

    chore: add `~/.config/fish/config.fish`
    Signed-off-by: David Xia <david@davidxia.com>

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,8 @@
+fish_config theme choose my
+
+# needed to use gpg-agent for SSH
+if status --is-interactive
+  set -x GPG_TTY (tty)
+  set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+  gpgconf --launch gpg-agent
+end
