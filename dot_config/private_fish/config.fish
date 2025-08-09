68ac4eb1ab55d84be4f364c1d6653bcd8ab6ad32
David Xia <david@davidxia.com>
2025-08-09 09:59:13 -0400

    Fix SSH and GnuPG agent forwarding
    Signed-off-by: David Xia <david@davidxia.com>

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,6 +3,7 @@ fish_config theme choose my
 # needed to use gpg-agent for SSH
 if status --is-interactive
   set -x GPG_TTY (tty)
-  set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+  # Only needed on local workstation
+  # set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
   gpgconf --launch gpg-agent
 end
